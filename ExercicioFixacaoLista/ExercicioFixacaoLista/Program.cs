86b2ae1a473f65426e01bf9ca6e9012d6099dbf3
Valniria Bandeira <xvalniria@gmail.com>
2020-03-12 20:02:48 -0300

    Execises of Lists

---
+++ ExercicioFixacaoLista/ExercicioFixacaoLista/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ExercicioFixacaoLista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many employees will be registered? ");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Employee> list = new List<Employee>();
+            int id;
+            string name;
+            double salary;
+
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Employee #" + (i+1) + ":");
+                Console.Write("Id: ");
+                id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                name = Console.ReadLine();
+                Console.Write("Salary: ");
+                salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                list.Add(new Employee(id, name, salary));
+            }
+            Console.WriteLine();
+
+            Console.Write("Enter the employee id that will have salary increase: ");
+            int searchId = int.Parse(Console.ReadLine());
+
+            Employee emp = list.Find(x => x.Id == searchId);
+
+            if(emp != null)
+            {
+                Console.Write("Enter the percentage: ");
+                double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                emp.IncreaseSalary(percentage);
+            }
+            else
+            {
+                Console.WriteLine("This id does not exist!");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Updated list of employees:");
+            foreach(Employee obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+
+        }
+    }
+}
---
+++ List/List/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+
+            list.Add("Maria");
+            list.Add("Alex");
+            list.Add("Bob");
+            list.Add("Ana");
+            list.Insert(0, "Valniria");
+
+
+            foreach(string obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+
+            Console.WriteLine("List count: " + list.Count);
+
+            string s1 = list.Find(x => x[0] == 'A');
+            Console.WriteLine(s1);
+            Console.WriteLine("-------------------");
+            string s2 = list.FindLast(x => x[0] == 'A');
+            Console.WriteLine(s2);
+            Console.WriteLine("-------------------");
+            int pos1 = list.FindIndex(x => x[0] == 'A');
+            Console.WriteLine("Posição da lista: " + pos1);
+            int pos2 = list.FindLastIndex(x => x[0] == 'A');
+            Console.WriteLine("Posição da lista: " + pos2);
+            Console.WriteLine("-------------------");
+            List<string> list2 = list.FindAll(x => x.Length == 3);
+            Console.WriteLine("-------------------");
+            foreach(string obj in list2)
+            {
+                Console.WriteLine(obj);
+            }
+            list.Remove("Alex");
+            Console.WriteLine("-------------------");
+            foreach (string obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+            list.RemoveAll(x => x[0] == 'M');
+            Console.WriteLine("-------------------");
+            foreach (string obj in list2)
+            {
+                Console.WriteLine(obj);
+            }
+
+            list.RemoveAt(1);
+            Console.WriteLine("-------------------");
+            foreach (string obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+            list.RemoveRange(1, 2);
+            Console.WriteLine("-------------------");
+            foreach (string obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
