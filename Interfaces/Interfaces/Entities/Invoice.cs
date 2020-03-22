8f17dff072384fbd441b34365c80d5400cb29e01
Valniria Bandeira <xvalniria@gmail.com>
2020-03-22 10:19:15 -0300

    Adding new programs

---
+++ Interfaces/Interfaces/Entities/Invoice.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace Interfaces.Entities
+{
+    class Invoice
+    {
+        public double BasicPayments { get; set; }
+        public double Tax { get; set; }
+
+
+        public Invoice()
+        {
+
+        }
+        public Invoice(double basicPayments, double tax)
+        {
+            BasicPayments = basicPayments;
+            Tax = tax;
+        }
+
+        public double TotalPayment
+        {
+            get { return BasicPayments + Tax; }
+        }
+
+        public override string ToString()
+        {
+            return "Basic Payments "
+                + BasicPayments.ToString("F2", CultureInfo.InvariantCulture)
+                + "\nTax: "
+                + Tax.ToString("F2", CultureInfo.InvariantCulture)
+                + "\nTotal Payment: "
+                + TotalPayment.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
