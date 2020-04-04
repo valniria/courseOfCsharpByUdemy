f9e83f09edafc9184dc066ca9767b60ceddf25b2
Valniria Bandeira <xvalniria@gmail.com>
2020-04-04 09:03:01 -0300

    Delegate Multicast

---
+++ DelegateMulticast/DelegateMulticast/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using DelegateMulticast.Services;
+
+namespace DelegateMulticast
+{
+    class Program
+    {
+        delegate double BinaryNumericOperation(double n1, double n2);
+        delegate void VoidBinaryOperation(double n1, double n2);
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation operation = CalculationService.Sum;
+
+            double result = operation(a, b);
+
+            Console.WriteLine(result);
+
+            VoidBinaryOperation voidOperation = CalculationService.ShowMax;
+            voidOperation += CalculationService.ShowSum;
+
+            voidOperation(a, b);
+
+            Console.WriteLine(voidOperation);
+
+        }
+    }
+}
