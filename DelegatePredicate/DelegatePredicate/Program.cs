4e8f187d65ca9023e43e437adb34bcb6f6651072
Valniria Bandeira <xvalniria@gmail.com>
2020-04-04 08:12:13 -0300

    Make a Deleagate with Predicate

---
+++ DelegatePredicate/DelegatePredicate/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using DelegatePredicate.Entities;
+using System.Collections.Generic;
+
+namespace DelegatePredicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            list.RemoveAll(x => x.Price >= 100.0);
+
+            foreach(Product product in list)
+            {
+                Console.WriteLine(product);
+            }
+        }
+    }
+}
