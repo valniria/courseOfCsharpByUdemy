f6b1ef837688476b0619f54f5382538b003736d6
Valniria Bandeira <xvalniria@gmail.com>
2020-04-05 15:22:53 -0300

    Alternative way

--- LinqWithLambda/LinqWithLambda/Program.cs
+++ LinqWithLambda/LinqWithLambda/Program.cs
@@ -28,26 +28,56 @@ namespace LinqWithLambda
                 new Product() { Id = 11, Name = "Level", Price = 70.0, Category = c1 }
             };
 
-            var r1 = products.Where(x => x.Category.Tier == 1 && x.Price < 900.0);
-
+            //var r1 = products.Where(x => x.Category.Tier == 1 && x.Price < 900.0);
+            var r1 =
+                from p in products
+                where p.Category.Tier == 1 && p.Price < 900.0
+                select p;
             Print("Show Products where Tier 1 and Price < 900.00", r1);
 
-            var r2 = products.Where(x => x.Category.Name == "Tools").Select(x => x.Name);
+            // var r2 = products.Where(x => x.Category.Name == "Tools").Select(x => x.Name);
+            var r2 =
+                from p in products
+                where p.Category.Name == "Tools"
+                select p.Name;
             Print("Show only Name of Products from Tools", r2);
 
-            var r3 = products.Where(x => x.Name[0] == 'C').Select(x => new { x.Name, x.Price, CategoryName = x.Category.Name });
+            //var r3 = products.Where(x => x.Name[0] == 'C').Select(x => new { x.Name, x.Price, CategoryName = x.Category.Name });
+            var r3 =
+                from p in products
+                where p.Name[0] == 'C'
+                select new { p.Name, p.Price, CategoryName = p.Category.Name }; 
             Print("Show Product where the inicial caracter is a 'C' And anonymous object", r3);
 
-            var r4 = products.Where(x => x.Category.Tier == 1).OrderBy(x => x.Price).ThenBy(x => x.Name);
+            //var r4 = products.Where(x => x.Category.Tier == 1).OrderBy(x => x.Price).ThenBy(x => x.Name);
+            var r4 =
+                from p in products
+                where p.Category.Tier == 1
+                orderby p.Name
+                orderby p.Price
+                select p;
             Print("Show Products where Tier = 1 order by Price then by name", r4);
 
-            var r5 = r4.Skip(2).Take(4);
+            //var r5 = r4.Skip(2).Take(4);
+            var r5 = 
+                (from p in r4
+                select p)
+                .Skip(2)
+                .Take(4);
             Print("Show Products where Tier = 1 order by Price then by name AND skip 2 and take 4", r5);
 
-            var r6 = products.FirstOrDefault();
+            //var r6 = products.FirstOrDefault();
+            var r6 = 
+                (from p in products select p)
+                .FirstOrDefault();
             Console.WriteLine("First of Products: " + r6);
 
-            var r7 = products.Where(x => x.Price > 3000.0).FirstOrDefault();
+            //var r7 = products.Where(x => x.Price > 3000.0).FirstOrDefault();
+            var r7 = 
+                (from p in products
+                 where p.Price > 3000.0
+                 select p)
+                 .FirstOrDefault();
             Console.WriteLine("First or default of Price more than 3000.0" + r7);
             Console.WriteLine();
 
@@ -75,7 +105,10 @@ namespace LinqWithLambda
             var r15 = products.Where(x => x.Category.Id == 1).Select(x => x.Price).Aggregate(0.0, (x, y) => x + y);
             Console.WriteLine("Select with Aggregate: " + r15);
 
-            var r16 = products.GroupBy(x => x.Category);
+            //var r16 = products.GroupBy(x => x.Category);
+            var r16 =
+                from p in products
+                group p by p.Category;
             foreach(IGrouping<Category, Product> group in r16)
             {
                 Console.WriteLine();
