6c6b0be205a8fc3e2dc1b358cb7131bf0cf371cc
Valniria Bandeira <xvalniria@gmail.com>
2020-04-05 11:09:09 -0300

    Learn more about Linq

---
+++ ExpressionaLinq/ExpressionaLinq/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace ExpressionaLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Specify a data source
+            int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+
+            // Define the query expression
+            var result = numbers
+                .Where(x => x % 2 == 0)
+                .Select(x => x * 10);
+
+            // Execute the query
+            foreach(int x in result)
+            {
+                Console.WriteLine(x);
+            }
+
+        }
+    }
+}
